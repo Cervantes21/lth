3e675d271b39f0ebf18260cebd3c81d00871ec71
cervantes21 <admon.owlsoul@gmail.com>
2025-01-09 05:31:34 -0600

    Check error O-RD-P/garantias-y-ajustes V2.00 01/09/25

--- app/(card links)/nosotros/page.js
+++ app/(card links)/nosotros/page.js
@@ -1,92 +1,133 @@
 import { Cards } from "@/components/Cards";
 import { MoreInfoCard } from "@/components/MoreInfoCard";
 import { Whatsapp } from "@/components/Whatsapp";
-import Footer from "@/components/Footer"
+import Footer from "@/components/Footer";
 import Link from "next/link";
-export default function Home() {
-    return (
-        <>
-            <MoreInfoCard page="nosotros" text="Batería En Casa es la empresa creada para ti." />
 
-            <div className="bg-grey-lth text-center uppercase font-medium py-4 mt-10">
-                <h2 className="text-xl lg:text-2xl max-w-[90%] mx-auto">Acerca de nosotros</h2>
-            </div>
-            <p className="mt-10 mb-4 px-4 text-center text-lg font-light sm:font-normal lg:text-xl lg:max-w-4xl lg:mx-auto">
-                <strong className="text-blue-lth">BEC Batería en Casa</strong> es una empresa de <strong className="text-blue-lth">instalación y reemplazo de baterías</strong> de automóviles, equipo pesado y otras aplicaciones.
+export default function Home() {
+  return (
+    <>
+      <MoreInfoCard page="nosotros" text="Batería En Casa es la empresa creada para ti." />
+
+      {/* Sección de encabezado */}
+      <div className="bg-grey-lth text-center uppercase font-medium py-4 mt-10">
+        <h2 className="text-xl lg:text-2xl max-w-[90%] mx-auto">
+          Acerca de nosotros
+        </h2>
+      </div>
+      <p className="mt-10 mb-4 px-4 text-center text-lg font-light sm:font-normal lg:text-xl lg:max-w-4xl lg:mx-auto">
+        <strong className="text-blue-lth">BEC Batería en Casa</strong> es una
+        empresa de{" "}
+        <strong className="text-blue-lth">
+          instalación y reemplazo de baterías
+        </strong>{" "}
+        de automóviles, equipo pesado y otras aplicaciones.
+      </p>
+
+      {/* Imagen animada o logotipo */}
+      <div className="flex justify-center my-6">
+        <Link href="/">
+          <img
+            src="/bec-animation.png"
+            alt="Animación de BEC Batería en Casa"
+            className="w-40 h-auto cursor-pointer transition-transform duration-300 hover:scale-105"
+          />
+        </Link>
+      </div>
+      <p className="mb-16 px-4 text-center text-lg font-light sm:font-normal lg:text-xl lg:max-w-4xl lg:mx-auto">
+        Trabajamos con las mejores marcas de baterias en México:{" "}
+        <strong className="text-blue-lth">LTH®</strong>,{" "}
+        <strong className="text-blue-lth"> LTH® PROTECT</strong>,{" "}
+        <strong className="text-blue-lth"> LTH® AGM EVOLUTION</strong>,{" "}
+        <strong className="text-blue-lth"> HITEC®</strong>,{" "}
+        <strong className="text-blue-lth"> FULLPOWER®</strong>,{" "}
+        <strong className="text-blue-lth"> ÁMERICA®</strong>, y más.
+      </p>
+
+      <div className="p-5 mt-5 md:mx-auto lg:w-full lg:max-w-4xl lg:text-xl">
+        <p className="mb-10 flex items-center gap-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 48 48"
+          >
+            <g fill="none" stroke="#d3172e" strokeLinejoin="round" strokeWidth="4">
+              <path d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z" />
+              <path strokeLinecap="round" d="m16 24l6 6l12-12" />
+            </g>
+          </svg>
+          <span>Entrega en menos de 90 minutos.</span>
+        </p>
+        <p className="mb-10 flex items-center gap-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 48 48"
+          >
+            <g fill="none" stroke="#d3172e" strokeLinejoin="round" strokeWidth="4">
+              <path d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z" />
+              <path strokeLinecap="round" d="m16 24l6 6l12-12" />
+            </g>
+          </svg>
+          <span>Diagnóstico profesional.</span>
+        </p>
+        <p className="flex items-center gap-x-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="1em"
+            height="1em"
+            viewBox="0 0 48 48"
+          >
+            <g fill="none" stroke="#d3172e" strokeLinejoin="round" strokeWidth="4">
+              <path d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z" />
+              <path strokeLinecap="round" d="m16 24l6 6l12-12" />
+            </g>
+          </svg>
+          <span>Instalación eficiente:</span>
+        </p>
+        <ul className="pl-10 list-disc space-y-2">
+          <li>Todas nuestras baterías tienen garantía y respaldo de la marca LTH.</li>
+          <li>Realizamos el diagnóstico del sistema eléctrico de tu vehículo.</li>
+          <li>Nuestro servicio es profesional y eficiente.</li>
+          <li>
+            Puedes pagar con tarjeta, efectivo o transferencia al término de la instalación
+            y/o diagnóstico.
+          </li>
+        </ul>
+        <p className="mt-10 italic text-gray-600">
+          *Pregunta por métodos alternos de pago.
+        </p>
+      </div>
+
+      {/* SECCIÓN DIAGONAL que redirige a /catalogo al hacer clic */}
+      <Link href="/catalogo">
+        <div className="diagonal-split-container cursor-pointer">
+          {/* Columna del texto */}
+          <div className="text-block">
+            <h2>SOMOS LA SOLUCIÓN PARA QUE TU DÍA NO SE DETENGA</h2>
+            <p>
+              Resolvemos el cambio de batería en tu casa, negocio u oficina. 
+              Llámanos y uno de nuestros técnicos te visitará para realizar 
+              el cambio de tu batería de manera rápida, eficiente y profesional.
             </p>
-            <div className="flex justify-center my-6">
-                <Link href="/">
-                    <img
-                        src="/bec-animation.png"
-                        alt="Animación de BEC Batería en Casa"
-                        className="w-40 h-auto cursor-pointer transition-transform duration-300 hover:scale-105"
-                    />
-                </Link>
-            </div>
-            <p className="mb-16 px-4 text-center text-lg font-light sm:font-normal lg:text-xl lg:max-w-4xl lg:mx-auto">
-                Trabajamos con las mejores marcas de baterias en México: <strong className="text-blue-lth">LTH®</strong>, Marcas que se acoplan a las necesidades de tu auto, <strong className="text-blue-lth">LTH® PROTECT</strong>, <strong className="text-blue-lth">LTH® AGM EVOLUTION</strong>, <strong className="text-blue-lth">HITEC®</strong>, <strong className="text-blue-lth">FULLPOWER®</strong>, <strong className="text-blue-lth">ÁMERICA®</strong>, y más.
-            </p>
-            <div className="p-5 mt-5 md:mx-auto lg:w-full lg:max-w-4xl lg:text-xl">
-                <p className="mb-10 flex items-center gap-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
-                        <g fill="none" stroke="#d3172e" strokeLinejoin="round" strokeWidth="4">
-                            <path d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z" />
-                            <path strokeLinecap="round" d="m16 24l6 6l12-12" />
-                        </g>
-                    </svg>
-                    <span>Entrega en menos de 90 minutos.</span>
-                </p>
-                <p className="mb-10 flex items-center gap-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
-                        <g fill="none" stroke="#d3172e" strokeLinejoin="round" strokeWidth="4">
-                            <path d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z" />
-                            <path strokeLinecap="round" d="m16 24l6 6l12-12" />
-                        </g>
-                    </svg>
-                    <span>Diagnóstico profesional.</span>
-                </p>
-                <p className="flex items-center gap-x-2">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" viewBox="0 0 48 48">
-                        <g fill="none" stroke="#d3172e" strokeLinejoin="round" strokeWidth="4">
-                            <path d="M24 44a19.937 19.937 0 0 0 14.142-5.858A19.937 19.937 0 0 0 44 24a19.938 19.938 0 0 0-5.858-14.142A19.937 19.937 0 0 0 24 4A19.938 19.938 0 0 0 9.858 9.858A19.938 19.938 0 0 0 4 24a19.937 19.937 0 0 0 5.858 14.142A19.938 19.938 0 0 0 24 44Z" />
-                            <path strokeLinecap="round" d="m16 24l6 6l12-12" />
-                        </g>
-                    </svg>
-                    <span>Instalación eficiente:</span>
-                </p>
-                <ul className="pl-10 list-disc space-y-2">
-                    <li>Todas nuestras baterías tienen garantía y respaldo de la marca LTH.</li>
-                    <li>Realizamos el diagnóstico del sistema eléctrico de tu vehículo.</li>
-                    <li>Nuestro servicio es profesional y eficiente.</li>
-                    <li>
-                        Puedes pagar con tarjeta, efectivo o transferencia al término de la instalación
-                        y/o diagnóstico.
-                    </li>
-                </ul>
-                <p className="mt-10 italic text-gray-600">*Pregunta por métodos alternos de pago.</p>
-            </div>
-            <div className="angled-container">
-              {/* Contenido dentro del div diagonal */}
-              <div className="content">
-                <h2>SOMOS LA SOLUCIÓN PARA QUE TU DÍA NO SE DETENGA</h2>
-                <p>
-                  Resolvemos el cambio de batería en tu casa, negocio o oficina. Llámanos y uno de nuestros técnicos te visitará para realizar el cambio de tu batería de manera rápida, eficiente y profesional.
-                </p>
-              </div>
-                
-              {/* Imagen */}
-              <div className="image">
-                <img
-                  src="/nosotros.png"
-                  alt="Cambio de batería"
-                  className="object-cover w-full h-full"
-                />
-              </div>
-            </div>
-                
-            <Cards page="nosotros" />
-            <Footer />
-            <Whatsapp />
-        </>
-    );
+          </div>
+
+          {/* Columna de la imagen */}
+          <div className="image-block">
+            <img 
+              src="/nosotros.png" 
+              alt="Cambio de batería" 
+            />
+          </div>
+        </div>
+      </Link>
+
+      {/* Resto de la página */}
+      <Cards page="nosotros" />
+      <Footer />
+      <Whatsapp />
+    </>
+  );
 }
--- app/garantias-y-ajustes/page.js
+++ app/garantias-y-ajustes/page.js
@@ -5,71 +5,113 @@ import Footer from "@/components/Footer";
 import Link from "next/link";
 
 export default function Home() {
-    return (
-        <>
-            <Link href="/" className="flex w-16 m-1 p-4 border-none outline-none">
-                <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="1em"
-                    height="1em"
-                    viewBox="0 0 24 24"
-                    className="w-10 h-10 arrow-back"
-                >
-                    <path
-                        className="duration-300 transition-all"
-                        fill="#d3172e"
-                        fillRule="evenodd"
-                        d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m-.47-13.53a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72H16a.75.75 0 0 0 0-1.5H9.81l1.72-1.72a.75.75 0 0 0 0-1.06"
-                        clipRule="evenodd"
-                    />
-                </svg>
-            </Link>
-            <h1 className="text-xl font-bold text-blue-lth uppercase w-full text-center mb-10 lg:text-3xl">Garantías y ajustes</h1>
-            <div className="bg-grey-lth text-center uppercase font-medium p-2">
-                <h2 className="w-96 mx-auto lg:w-fit lg:py-4 lg:text-xl">Información importante sobre la garantía</h2>
-            </div>
-            <p className="mt-10 mb-16 w-96 lg:w-full lg:max-w-[1300px] mx-auto text-center text-lg font-light lg:text-xl lg:font-normal">
-                Batería en Casa solo usa la mejor marca de batería en México: LTH. Cada batería que instalamos tiene garantía, de acuerdo a las políticas de nuestro proveedor.
-                Cuando nuestro técnico acuda a su domicilio, por procedimiento determinará si la batería necesita una recarga o un reemplazo. La recarga tiene un costo.
-            </p>
-            <BotonGarantia />
-            <div className="bg-grey-lth text-center uppercase font-medium p-2">
-                <h2 className="w-96 mx-auto lg:w-fit lg:py-4 lg:text-xl font-bold">Aviso de privacidad</h2>
-            </div>
-            <div className="mt-10 mb-16 w-96 lg:w-full lg:max-w-[1300px] mx-auto text-left text-sm font-light lg:text-base lg:font-normal space-y-4">
-                <p>
-                    Es política de Batería en Casa, sus filiales, subsidiarias, así como cualquier sociedad matriz o sociedad del grupo REMOSA (todas estas personas morales denominadas en lo sucesivo como “REMOSA DE CV”) y la red de Centros de Servicio y Venta, respetar y proteger la privacidad de todos nuestros clientes y potenciales clientes así como de todas las personas titulares de los datos (“Titular”) que tengamos en nuestra posesión, para lo cual las opciones y medios que hemos establecido para cuidar el tratamiento de su información son de manera general los mismos que utilizamos para nuestros propios datos y documentos.
-                </p>
-                <p>
-                    El tratamiento de los datos personales del “Titular”, comprende las siguientes finalidades:
-                </p>
-                <ul className="list-disc ml-5 space-y-2">
-                    <li>
-                        Ser contactado y enviar información a fin de conocer el nivel de satisfacción del “Titular” en relación con la adquisición de bienes o servicios que le hubiesen sido prestados por Batería en Casa y cualquier otra empresa del grupo REMOSA, así como dar seguimiento a sus peticiones, sugerencias, reclamaciones y/o cualquier otra solicitud del “Titular” o para realizar acciones de actualizaciones técnicas de los productos o servicios que hubiese adquirido.
-                    </li>
-                    <li>
-                        Ser contactado y enviar información para darle a conocer promociones, información de nuestros socios de negocios, ofrecerle nuestros productos o servicios, enviar publicidad respecto a productos y servicios de cualquiera de las marcas Batería en Casa y sus asociados comercializa, y demás que estas mismas ofrezcan, pudiendo realizar cualquier acción comercial, de mercadotecnia, estadística y de prospección tendiente a dichas finalidades.
-                    </li>
-                    <li>
-                        Ser contactado y enviar información para dar cumplimiento a requerimientos legales, mantener actualizados nuestros registros, dar seguimiento a nuestra relación comercial si existiese y mantener comunicación en general.
-                    </li>
-                </ul>
-                <p>
-                    Se ponen a su disposición los siguientes medios para ejercer los derechos de acceso, rectificación, cancelación, oposición (Solicitud ARCO) así como de revocación para el tratamiento de sus datos personales por Refaccionaria Moncada S.A. de C.V.:
-                </p>
-                <p>
-                    <strong>Domicilio:</strong> Av. Plan de Ayala 353 Colonia Amatitlán Cuernavaca Morelos C.P. 62410
-                </p>
-                <p>
-                    <strong>Correo electrónico:</strong> bateriaencasabec@gmail.com
-                </p>
-                <p>
-                    Le informamos que nuestro aviso de privacidad se encuentra en <a href="https://www.bateriaencasa.com" className="text-blue-lth underline">www.bateriaencasa.com/garantias-y-ajustes</a>
-                </p>
-            </div>
-            <Cards page="all" />
-            <Footer />
-            <Whatsapp />
-        </>
-    );
+  return (
+    <>
+      <Link href="/" className="flex w-16 m-1 p-4 border-none outline-none">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="1em"
+          height="1em"
+          viewBox="0 0 24 24"
+          className="w-10 h-10 arrow-back"
+        >
+          <path
+            className="duration-300 transition-all"
+            fill="#d3172e"
+            fillRule="evenodd"
+            d="M12 22C6.477 22 2 17.523 2 12S6.477 2 12 2s10 4.477 10 10s-4.477 10-10 10m-.47-13.53a.75.75 0 0 0-1.06 0l-3 3a.75.75 0 0 0 0 1.06l3 3a.75.75 0 1 0 1.06-1.06l-1.72-1.72H16a.75.75 0 0 0 0-1.5H9.81l1.72-1.72a.75.75 0 0 0 0-1.06"
+            clipRule="evenodd"
+          />
+        </svg>
+      </Link>
+
+      {/* Título principal */}
+      <h1 className="w-full text-center text-xl font-bold text-blue-lth uppercase mb-10 sm:text-2xl lg:text-3xl">
+        Garantías y ajustes
+      </h1>
+
+      {/* Sección: Información importante sobre la garantía */}
+      <div className="bg-grey-lth text-center uppercase font-medium p-2 sm:p-4">
+        <h2 className="w-96 mx-auto sm:w-full sm:text-lg md:text-xl lg:w-fit">
+          Información importante sobre la garantía
+        </h2>
+      </div>
+      <p className="mt-10 mb-16 w-96 mx-auto text-center text-lg font-light sm:w-full sm:px-4 sm:text-base md:text-lg lg:max-w-[1300px] lg:text-xl lg:font-normal">
+        Batería en Casa solo usa la mejor marca de batería en México: LTH. Cada
+        batería que instalamos tiene garantía, de acuerdo a las políticas de
+        nuestro proveedor. Cuando nuestro técnico acuda a su domicilio, por
+        procedimiento determinará si la batería necesita una recarga o un
+        reemplazo. La recarga tiene un costo.
+      </p>
+
+      {/* Botón de garantía */}
+      <div className="flex justify-center mb-8">
+        <BotonGarantia />
+      </div>
+
+      {/* Aviso de privacidad */}
+      <div className="bg-grey-lth text-center uppercase font-medium p-2 sm:p-4">
+        <h2 className="w-96 mx-auto text-base font-bold sm:w-full sm:text-lg md:text-xl lg:w-fit">
+          Aviso de privacidad
+        </h2>
+      </div>
+      <div className="mt-10 mb-16 w-96 mx-auto text-left text-sm font-light space-y-4 sm:w-full sm:px-4 md:text-base lg:max-w-[1300px] lg:font-normal">
+        <p>
+          Es política de Batería en Casa, sus filiales, subsidiarias, así como
+          cualquier sociedad matriz o sociedad del grupo REMOSA (todas estas
+          personas morales denominadas en lo sucesivo como “REMOSA DE CV”) y la
+          red de Centros de Servicio y Venta, respetar y proteger la privacidad
+          de todos nuestros clientes...
+        </p>
+        <p>El tratamiento de los datos personales del “Titular” comprende las siguientes finalidades:</p>
+        <ul className="list-disc ml-5 space-y-2">
+          <li>
+            Ser contactado y enviar información a fin de conocer el nivel de
+            satisfacción del “Titular” en relación con la adquisición de bienes
+            o servicios...
+          </li>
+          <li>
+            Ser contactado y enviar información para darle a conocer
+            promociones, información de nuestros socios de negocios, ofrecerle
+            nuestros productos o servicios...
+          </li>
+          <li>
+            Ser contactado y enviar información para dar cumplimiento a
+            requerimientos legales...
+          </li>
+        </ul>
+        <p>
+          Se ponen a su disposición los siguientes medios para ejercer los
+          derechos de acceso, rectificación, cancelación, oposición (Solicitud
+          ARCO) así como de revocación...
+        </p>
+        <p>
+          <strong>Domicilio:</strong> Av. Plan de Ayala 353 Colonia Amatitlán
+          Cuernavaca Morelos C.P. 62410
+        </p>
+        <p>
+          <strong>Correo electrónico:</strong> bateriaencasabec@gmail.com
+        </p>
+        <p>
+          Le informamos que nuestro aviso de privacidad se encuentra en{" "}
+          <a
+            href="https://www.bateriaencasa.com"
+            className="text-blue-lth underline"
+          >
+            www.bateriaencasa.com/garantias-y-ajustes
+          </a>
+        </p>
+      </div>
+      {/* Cards */}
+      <div className="w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mb-8">
+        <Cards page="all" />
+      </div>
+
+      {/* Footer */}
+      <Footer />
+
+      {/* Whatsapp */}
+      <Whatsapp />
+    </>
+  );
 }
