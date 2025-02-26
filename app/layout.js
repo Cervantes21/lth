244ec3ea213e87d28da19d83b5ea4eed2fb66f66
cervantes21 <admon.owlsoul@gmail.com>
2025-02-26 15:14:16 -0600

    fix layout

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,8 @@
 import { Inter } from "next/font/google";
-import Head from "next/head";
-import Script from "next/script";
+import "./globals.css";
 import Header from "@/components/Header";
 import HeaderDesktop from "@/components/HeaderDesktop";
+import Script from "next/script";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -35,20 +35,33 @@ const municipiosMorelos = [
   "TLAQUILTENANGO",
 ];
 
+export const metadata = {
+  title: "Batería en Casa",
+  description:
+    "Llevamos la batería a tu casa en cualquier momento en tan solo 90 minutos en Todo Morelos.",
+};
+
 export default function RootLayout({ children }) {
   const siteName = "Batería en Casa";
   const municipiosString = municipiosMorelos.join(" | ");
-  const pageTitle = `ENTREGAMOS BATERÍAS DE AUTO Y MOTO A DOMICILIO EN ${municipiosString} | Batería en Casa`;
+  const pageTitle = `ENTREGAMOS BATERÍAS DE AUTO Y MOTO A DOMICILIO EN ${municipiosString} | ${siteName}`;
   const pageDescription = `Entrega rápida de baterías de auto y moto a domicilio en ${municipiosString} y más municipios de Morelos. Contáctanos para un servicio confiable y eficiente.`;
-  const ogImage = "https://www.bateriaencasa.com/og-image.webp"; // Imagen para Open Graph y WhatsApp
+  const ogImage = "https://www.bateriaencasa.com/og-image.webp";
 
   return (
-    <>
-      <Head>
-        {/* Meta Tags */}
+    <html lang="en">
+      <head>
+        {/* Meta Tags Generales */}
+        <meta charSet="UTF-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="author" content="Batería en Casa Team" />
+
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
-        <meta name="keywords" content="baterías de auto, baterías de moto, servicio a domicilio, Morelos, Cuernavaca, Cuautla, Jiutepec" />
+        <meta
+          name="keywords"
+          content="baterías de auto, baterías de moto, servicio a domicilio, Morelos, Cuernavaca, Cuautla, Jiutepec"
+        />
 
         {/* Open Graph Meta Tags */}
         <meta property="og:type" content="website" />
@@ -59,6 +72,9 @@ export default function RootLayout({ children }) {
         <meta property="og:image:width" content="1200" />
         <meta property="og:image:height" content="630" />
         <meta property="og:site_name" content={siteName} />
+        <meta property="og:image:secure_url" content={ogImage} />
+        <meta property="og:image:alt" content="Miniatura para WhatsApp" />
+        <meta property="og:image:type" content="image/webp" />
 
         {/* Twitter Card */}
         <meta name="twitter:card" content="summary_large_image" />
@@ -66,17 +82,7 @@ export default function RootLayout({ children }) {
         <meta name="twitter:description" content={pageDescription} />
         <meta name="twitter:image" content={ogImage} />
         <meta name="twitter:site" content="@bateriaencasa_" />
-
-        {/* WhatsApp */}
-        <meta property="og:image:secure_url" content={ogImage} />
-        <meta property="og:image:alt" content="Miniatura para WhatsApp" />
-        <meta property="og:image:type" content="image/webp" />
-
-        {/* General Meta Tags */}
-        <meta name="author" content="Batería en Casa Team" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta charSet="UTF-8" />
-      </Head>
+      </head>
 
       <body className={inter.className}>
         {/* Google Tag Manager */}
@@ -95,15 +101,15 @@ export default function RootLayout({ children }) {
           `}
         </Script>
 
-        {/* Header Components */}
+        {/* Componentes de cabecera */}
         <Header />
         <HeaderDesktop />
 
-        {/* Main Content */}
+        {/* Contenido principal */}
         <main className="pt-1 lg:pt-[80px]">
           {children}
         </main>
       </body>
-    </>
+    </html>
   );
 }
